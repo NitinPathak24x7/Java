2cfb2985544a19b5d4fb3913163966e2565c94d1
NitinPathak24x7 <nitinpathak24x7@gmail.com>
2026-05-04 00:16:59 +0530

    Assignment 1

---
+++ Java_Assignment/Assign 1/Q5.java
@@ -0,0 +1,9 @@
+public class Q5 {
+    public static void main (String [] args){
+        
+        int x=5;
+
+        System.out.println("The value of Variable is "+ x);
+
+    }
+}
---
+++ Java_Assignment/Assign 1/Q6.java
@@ -0,0 +1,7 @@
+public class Q6 {
+    public static void main (String [] args){
+        float x=3.5f;
+        System.out.println("The value of float is : "+x);
+
+    }
+}
